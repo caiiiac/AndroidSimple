d6d313e43767c6285c649993749a7bfe6a7294cf
caijq <caiiiac@163.com>
2018-11-30 17:03:45 +0800

    listView

--- app/src/main/java/com/simple/caiiiac/androidsimple/MainActivity.java
+++ app/src/main/java/com/simple/caiiiac/androidsimple/MainActivity.java
@@ -2,12 +2,31 @@ package com.simple.caiiiac.androidsimple;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    private MyAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        adapter = new MyAdapter(this);
+
+        ListView listView =  (ListView) findViewById(R.id.listView);
+        listView.setAdapter(adapter);
+
+        ArrayList<String> datas = new ArrayList<>();
+        for (int i = 0; i < 20; i++) {
+            datas.add("第" + i + "行");
+        }
+
+        adapter.setDatas(datas);
+        adapter.notifyDataSetChanged();
     }
 }
---
+++ app/src/main/java/com/simple/caiiiac/androidsimple/MyAdapter.java
@@ -0,0 +1,52 @@
+package com.simple.caiiiac.androidsimple;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+import java.util.ArrayList;
+
+public class MyAdapter extends BaseAdapter {
+
+    private ArrayList<String> mDatas;
+    private LayoutInflater mInflater;
+
+    public MyAdapter(Context c) {
+        mInflater = LayoutInflater.from(c);
+    }
+
+    @Override
+    public int getCount() {
+        return mDatas!= null ? mDatas.size() : 0;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        TextView textView = null;
+        if (convertView == null) {
+            convertView = mInflater.inflate(android.R.layout.simple_list_item_1, null);
+            textView = convertView.findViewById(android.R.id.text1);
+            convertView.setTag(textView);
+        } else {
+            textView = (TextView) convertView.getTag();
+        }
+        textView.setText(mDatas.get(position));
+        return convertView;
+    }
+
+    public void setDatas(ArrayList<String> datas) {
+        mDatas = datas;
+    }
+}
