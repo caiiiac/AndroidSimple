26ef0b5fa7c7bcb1875cbc6a9c2791167aaba5c1
caijq <caiiiac@163.com>
2018-11-30 15:54:41 +0800

    layout

--- app/src/main/java/com/simple/caiiiac/androidsimple/MainActivity.java
+++ app/src/main/java/com/simple/caiiiac/androidsimple/MainActivity.java
@@ -2,6 +2,8 @@ package com.simple.caiiiac.androidsimple;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        RadioGroup radioGroup = (RadioGroup) findViewById(R.id.sex_rg);
+
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                if (checkedId == R.id.radio_btn_boy) {
+                    Toast.makeText(MainActivity.this,"男",Toast.LENGTH_SHORT).show();
+                } else if (checkedId == R.id.radio_btn_gril) {
+                    Toast.makeText(MainActivity.this,"女",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
