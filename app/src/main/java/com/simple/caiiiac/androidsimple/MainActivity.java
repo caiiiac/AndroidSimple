a3bbf337c3c1b209f90da491f66e321bb38fc9f9
caijq <caiiiac@163.com>
2018-12-04 11:51:23 +0800

    单选框

--- app/src/main/java/com/simple/caiiiac/androidsimple/MainActivity.java
+++ app/src/main/java/com/simple/caiiiac/androidsimple/MainActivity.java
@@ -1,78 +1,60 @@
 package com.simple.caiiiac.androidsimple;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.net.Uri;
 import android.os.Bundle;
-import android.util.JsonReader;
-import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
 
-
-import org.xutils.common.Callback;
-import org.xutils.http.RequestParams;
 import org.xutils.view.annotation.ContentView;
 import org.xutils.view.annotation.Event;
-import org.xutils.x;
 
-import java.util.HashMap;
 
 @ContentView(R.layout.activity_main)
 public class MainActivity extends BaseActivity {
 
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_main);
-//        x.view().inject(this);
-
 
     }
 
-    @Event(value = R.id.getBtn)
-    private void onClickDownload(View view) {
-        Log.d("caiiiac","点击了");
-        RequestParams params = new RequestParams(top250());
-//        params.addQueryStringParameter("wd","android");
-
-
-        x.http().get(params, new Callback.CommonCallback<HashMap>() {
-
-//            @Override
-//            public void onSuccess(String result) {
-//                Log.d("caiiiac","success");
-//            }
-
-            @Override
-            public void onSuccess(HashMap result) {
-                Log.d("caiiiac","success");
-            }
-
-            @Override
-            public void onError(Throwable ex, boolean isOnCallback) {
-                Log.d("caiiiac","e");
-            }
-
-            @Override
-            public void onCancelled(CancelledException cex) {
-                Log.d("caiiiac","c");
-            }
-
-            @Override
-            public void onFinished() {
-                Log.d("caiiiac","f");
-            }
-        });
-    }
-
-    public void get() {
+    // 普通样式
+    @Event(value = R.id.normalDialog)
+    private void openDialog(View view) {
+        new AlertDialog.Builder(this)
+                .setTitle("提示")
+                .setMessage("这是一个对话框")
+                .setPositiveButton("确定", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(MainActivity.this,"点击了确定", Toast.LENGTH_SHORT).show();
+//                        MainActivity.this.finish();
+                    }
+                })
+                .setNegativeButton("取消", null)
+                .create()
+                .show();
 
     }
 
-    public String top250() {
-        return "http://api.douban.com/v2/movie/top250";
+    // 单选框
+    String[] sexs = new String[]{"男", "女"};
+    @Event(value = R.id.singleDialog)
+    private void openSingleDialog(View view) {
+        new AlertDialog.Builder(this)
+                .setTitle("请选择您的性别")
+                .setSingleChoiceItems(sexs, 0, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(MainActivity.this, sexs[which], Toast.LENGTH_SHORT).show();
+                        dialog.dismiss();
+                    }
+                })
+//                .setPositiveButton("确定", null)
+                .create()
+                .show();
     }
 
-    public String baidu() {
-        return "https://www.baidu.com/s";
-    }
 }
