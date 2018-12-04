0ca64cd603b271f982b85690f8ec2d4f3f1c6ae1
caijq <caiiiac@163.com>
2018-12-04 10:40:50 +0800

    xUntil

--- app/src/main/java/com/simple/caiiiac/androidsimple/MainActivity.java
+++ app/src/main/java/com/simple/caiiiac/androidsimple/MainActivity.java
@@ -1,108 +1,78 @@
 package com.simple.caiiiac.androidsimple;
 
-import android.content.SharedPreferences;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.text.TextUtils;
+import android.util.JsonReader;
+import android.util.Log;
 import android.view.View;
-import android.widget.EditText;
-import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
 
-    private EditText mAccountET;
-    private EditText mPasswodkET;
+import org.xutils.common.Callback;
+import org.xutils.http.RequestParams;
+import org.xutils.view.annotation.ContentView;
+import org.xutils.view.annotation.Event;
+import org.xutils.x;
 
-    private ContactDao mDao;
+import java.util.HashMap;
+
+@ContentView(R.layout.activity_main)
+public class MainActivity extends BaseActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+//        setContentView(R.layout.activity_main);
+//        x.view().inject(this);
 
-        mAccountET = this.<EditText>findViewById(R.id.account_et);
-        mPasswodkET = this.<EditText>findViewById(R.id.password_et);
-
-        readInfo();
-
-        mDao = new ContactDao(this);
 
     }
 
-    // 登录事件
-    public void loginClick(View v) {
-        String account = mAccountET.getText().toString();
-        String password = mPasswodkET.getText().toString();
+    @Event(value = R.id.getBtn)
+    private void onClickDownload(View view) {
+        Log.d("caiiiac","点击了");
+        RequestParams params = new RequestParams(top250());
+//        params.addQueryStringParameter("wd","android");
 
-        if (TextUtils.isEmpty(account) || TextUtils.isEmpty(password)) {
-            Toast.makeText(this,"请填写完整的信息",Toast.LENGTH_SHORT).show();
-            return;
-        }
-        saveInfo(account,password);
-    }
 
-    // 保存数据
-    private void saveInfo(String account, String password) {
+        x.http().get(params, new Callback.CommonCallback<HashMap>() {
 
-        SharedPreferences sp = getSharedPreferences("userInfo", MODE_PRIVATE);
-        SharedPreferences.Editor edit = sp.edit();
-        edit.putString("account", account);
-        edit.putString("password", password);
-        edit.commit();
-
-        sp.edit();
-//        try {
-//            File file = new File(getCacheDir(),"/info.txt");
-//            BufferedWriter writer = new BufferedWriter(new FileWriter(file));
-//            writer.write(account+"#"+password);
-//            writer.close();
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-    }
-
-    // 读取数据
-    private void readInfo() {
-        SharedPreferences sp = getSharedPreferences("userInfo", MODE_PRIVATE);
-        String account = sp.getString("account","");
-        String password = sp.getString("password","");
-
-        mAccountET.setText(account);
-        mPasswodkET.setText(password);
-
-//        try {
-//            File file = new File(getCacheDir(), "/info.text");
-//            if (file.exists() && file.length() > 0) {
-//                BufferedReader reader = new BufferedReader(new FileReader(file));
-//                String line = reader.readLine();
-//                if (line.contains("#")) {
-//                    String[] datas = line.split("#");
-//                    mAccountET.setText(datas[0]);
-//                    mPasswodkET.setText(datas[1]);
-//                }
+//            @Override
+//            public void onSuccess(String result) {
+//                Log.d("caiiiac","success");
 //            }
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
+
+            @Override
+            public void onSuccess(HashMap result) {
+                Log.d("caiiiac","success");
+            }
+
+            @Override
+            public void onError(Throwable ex, boolean isOnCallback) {
+                Log.d("caiiiac","e");
+            }
+
+            @Override
+            public void onCancelled(CancelledException cex) {
+                Log.d("caiiiac","c");
+            }
+
+            @Override
+            public void onFinished() {
+                Log.d("caiiiac","f");
+            }
+        });
     }
 
-    public void insertContact(View v) {
-        boolean isSucceed = mDao.insertContact(mAccountET.getText().toString(), mPasswodkET.getText().toString());
-        Toast.makeText(this, isSucceed ? "插入成功" : "插入失败", Toast.LENGTH_SHORT).show();
+    public void get() {
+
     }
 
-    public void updateContact(View v) {
-        boolean isSucceed = mDao.updateContact(mAccountET.getText().toString(), mPasswodkET.getText().toString());
-        Toast.makeText(this, isSucceed ? "更新成功" : "更新失败", Toast.LENGTH_SHORT).show();
+    public String top250() {
+        return "http://api.douban.com/v2/movie/top250";
     }
 
-    public void deleteContact(View v) {
-        boolean isSucceed = mDao.deleteContact(mAccountET.getText().toString());
-        Toast.makeText(this, isSucceed ? "删除成功" : "删除失败", Toast.LENGTH_SHORT).show();
-    }
-
-    public void queryContact(View v) {
-        String result = mDao.queryContact(mPasswodkET.getText().toString());
-        Toast.makeText(this, result, Toast.LENGTH_SHORT).show();
+    public String baidu() {
+        return "https://www.baidu.com/s";
     }
 }
---
+++ app/src/main/java/com/simple/caiiiac/androidsimple/MyApplication.java
@@ -0,0 +1,23 @@
+package com.simple.caiiiac.androidsimple;
+
+import android.app.Application;
+
+import org.xutils.x;
+
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.SSLSession;
+
+public class MyApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        x.Ext.init(this);
+
+        x.Ext.setDefaultHostnameVerifier(new HostnameVerifier() {
+            @Override
+            public boolean verify(String hostname, SSLSession session) {
+                return true;
+            }
+        });
+    }
+}
