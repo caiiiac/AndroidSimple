960c241d225118ee83a46bd24405859b51d19ee5
caijq <caiiiac@163.com>
2018-12-05 10:48:21 +0800

    短信

--- app/src/main/java/com/simple/caiiiac/androidsimple/MainActivity.java
+++ app/src/main/java/com/simple/caiiiac/androidsimple/MainActivity.java
@@ -1,8 +1,8 @@
 package com.simple.caiiiac.androidsimple;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
-import android.os.PersistableBundle;
 import android.view.View;
 
 import org.xutils.view.annotation.ContentView;
@@ -20,12 +20,19 @@ public class MainActivity extends BaseActivity {
     }
 
     @Event(value = R.id.openSecond)
-    private void onClickDownload(View view) {
+    private void onClickSecond(View view) {
 
         Intent second = new Intent(this, SecondActivity.class);
         second.putExtra("title", "第二层界面");
         second.putExtra("person", new Person("二层楼", 18));
         startActivity(second);
+    }
 
+    @Event(value = R.id.openSMS)
+    private void onClickSMS(View view) {
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(Uri.parse("smsto:10000"));
+        intent.putExtra("sms_body", "这是短信内容");
+        startActivity(intent);
     }
 }
