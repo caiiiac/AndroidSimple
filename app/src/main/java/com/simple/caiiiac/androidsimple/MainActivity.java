10773e7bd755757218a6c76e7a58ee86ac27f104
caijq <caiiiac@163.com>
2019-03-06 17:22:15 +0800

    mqtt

--- app/src/main/java/com/simple/caiiiac/androidsimple/MainActivity.java
+++ app/src/main/java/com/simple/caiiiac/androidsimple/MainActivity.java
@@ -22,6 +22,7 @@ public class MainActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        MqttMsgService.startService(this);
     }
 
 
